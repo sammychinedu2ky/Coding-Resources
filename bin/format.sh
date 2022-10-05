acc07dbe165a197ce7aee459f188ec9f721189dd
Adophilus <uchenna19of@gmail.com>
2022-10-05 15:34:15 +0100

    debugging formatter

--- bin/format.sh
+++ bin/format.sh
@@ -1,17 +1,67 @@
 #! /usr/bin/env bash
 
 function main () {
-  local RESOURCES_FOLDER="resources"
-  local resourceFolders=($(ls $RESOURCES_FOLDER))
+  function compile () {
+    local sourceContent="$1"
+    local old_IFS="$IFS"
+    #IFS='
+#'
+    # IFS=$old_IFS
+    echo "${sourceContent}" | sed -E 's/.*\[(.*?)\]\((.*?)\)/{ "type": "pdf", "title": "\1", "link": "\2" }/'
 
-  for resourceName in "${resourceFolders[@]}"
+  }
+
+  local old_IFS="$IFS"
+  IFS='
+'
+  local RESOURCES_FOLDER="resources"
+  local resourceFolders=($(ls "${RESOURCES_FOLDER}"))
+
+  for resourceFolder in "${resourceFolders[@]}"
   do
-    for resourceFile in "${resourceFiles[@]}"
+    local resourceFolder="Git"
+    local currentFolder="${RESOURCES_FOLDER}/${resourceFolder}"
+    local resourceFiles=($(ls "${currentFolder}" | grep -E ".md$"))
+
+    compiledOutput="["
+
+    for i in $(seq 0 $((${#resourceFiles[@]} - 1)))
     do
-      local fileContent=$(cat "$resourceFile")
-      echo $fileContent
+      local resourceFile="${resourceFiles[i]}"
+
+      IFS=$_old_IFS
+      local fileContent="$(cat "${RESOURCES_FOLDER}/${resourceFolder}/${resourceFile}" | grep -E '\[.*\]\(.*\)|\(.*\)\[.*\]')"
+
+      local compiled="$(compile "$fileContent")"
+
+      if [ -n "${compiled}" ]
+      then
+        if [[ "${#resourceFiles[@]}" == "1" ]]
+        then
+          compiledOutput+="${compiled}"
+        else
+          if [[ $i == $((${#resourceFiles[@]} - 1)) ]]
+          then
+            compiledOutput+="${compiled}"
+          else
+            compiledOutput+="${compiled},"
+          fi
+        fi
+      fi
+
+      IFS='
+'
     done
+
+    compiledOutput+="]"
+
+    echo $compiledOutput | jq
+    echo $compiledOutput
+    echo
+
   done
+
+  IFS=$old_IFS
 }
 
 main
