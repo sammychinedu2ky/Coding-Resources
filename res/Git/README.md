b52e4e4ff07de7cfad565f8ca11bef852eb2d2f5
Adophilus <uchenna19of@gmail.com>
2022-10-05 09:58:09 +0100

    add style to resource description

--- res/Git/README.md
+++ res/Git/README.md
@@ -1,8 +1,18 @@
+
 # Git
 
-    ## Index
-      - PDF
-    - [LinkedIn learning basics](https://www.linkedin.com/learning/git-essential-training-the-basics)
+### Index
+- [Courses](#courses)
+- [Games](#games)"    
+- [PDFs](#pdfs)
+
+## Courses
+- [LinkedIn learning basics](https://www.linkedin.com/learning/git-essential-training-the-basics)
 - [LinkedIn learning intermediate](https://www.linkedin.com/learning/git-intermediate-techniques)
+
+## Games
 - [Git learning games](https://learngitbranching.js.org/)
+
+## PDFs
 - [Pro Git](https://git-scm.com/book/en/v2)
+  __The official Pro Git Ebook__
--- src/main.py
+++ src/main.py
@@ -4,20 +4,47 @@ import os
 RESOURCES_FOLDER = "res"
 # RESOURCES_FOLDER = "resources"
 RESOURCE_HEADING = """
+# {resourceName}
 
-
+### Index
+- [Courses](#courses)
+- [Games](#games)
+- [PDFs](#pdfs)
 """
-def compile(resourceName, resourceStucture) -> str:
-    compiledOutput = f"# {resourceName}\n"
-    compiledOutput += f"## Index\n"
-    compiledOutput += f"- [Courses](#courses)"
-    compiledOutput += f"- [Games](#games)"
-    compiledOutput += f"- [PDFs](#pdfs)"
 
-    for resource in resourceStucture:
-        compiledOutput += f"- [{resource['title']}]({resource['link']})\n"
+def compile(resourceName, resourceStucture) -> str:
+    def compileResource (resource):
+        compiledOutput = f"- [{resource['title']}]({resource['link']})\n"
         if resource.get('description'):
-            compiledOutput += f"  __{resource['description']}__"
+            compiledOutput += f"  __{resource['description']}__\n"
+        return compiledOutput
+
+    compiledOutput = RESOURCE_HEADING.format(resourceName = resourceName)
+    resources = {
+        "courses": [],
+        "games": [],
+        "pdfs": []
+    }
+    for resource in resourceStucture:
+        if (resource["type"] == "course"):
+            resources["courses"].append(resource)
+        elif (resource["type"] == "game"):
+            resources["games"].append(resource)
+        elif (resource["type"] == "pdf"):
+            resources["pdfs"].append(resource)
+
+    compiledOutput += "\n## Courses\n"
+    for resource in resources["courses"]:
+        compiledOutput += compileResource(resource)
+
+    compiledOutput += "\n## Games\n"
+    for resource in resources["games"]:
+        compiledOutput += compileResource(resource)
+
+    compiledOutput += "\n## PDFs\n"
+    for resource in resources["pdfs"]:
+        compiledOutput += compileResource(resource)
+
     return compiledOutput
 
 if __name__ == '__main__':
